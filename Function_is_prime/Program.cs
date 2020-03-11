7dacf2475dee905b2180423b240917c28cfa5bac
“edson.dossantos” <edson.lopes@gmx.de>
2020-03-11 14:39:08 -0400

    Function_is_prime_Git_Hub

---
+++ Function_is_prime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Function_is_prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Staring Project here - GitHub
+      
+        }
+
+        public static bool EstPremier(int nombre)
+        {
+            int nbDivisions = 0;
+
+            for (int i = 1; i <= nombre; i++)
+            {
+                if (nombre % i == 0)
+                {
+                    nbDivisions++;
+                }
+
+            }
+            return nbDivisions == 2;
+        }
+    }
+}
